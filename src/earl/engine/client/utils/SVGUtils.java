465f9d6a6317991246495bdca5fc3955d5e52bc6
rzymek <rzymek@gmail.com>
2012-10-28 22:19:36 +0100

    dead poll, admin

---
+++ src/earl/engine/client/utils/SVGUtils.java
@@ -0,0 +1,31 @@
+package earl.engine.client.utils;
+
+import org.vectomatic.dom.svg.OMSVGElement;
+import org.vectomatic.dom.svg.OMSVGGElement;
+import org.vectomatic.dom.svg.OMSVGMatrix;
+import org.vectomatic.dom.svg.OMSVGPathElement;
+import org.vectomatic.dom.svg.OMSVGRect;
+
+public class SVGUtils {
+
+	public static OMSVGRect getBBox(OMSVGElement e) {
+		if (e instanceof OMSVGGElement) {
+			return ((OMSVGGElement) e).getBBox();
+		} else if (e instanceof OMSVGPathElement) {
+			return ((OMSVGPathElement) e).getBBox();
+		} else {
+			throw new RuntimeException("SVGUtils.getBBox: unsupported " + e.getClass().getName());
+		}
+	}
+
+	public static OMSVGMatrix getTransformToElement(OMSVGElement e, OMSVGElement to) {
+		if (e instanceof OMSVGGElement) {
+			return ((OMSVGGElement) e).getTransformToElement(to);
+		} else if (e instanceof OMSVGPathElement) {
+			return ((OMSVGPathElement) e).getTransformToElement(to);
+		} else {
+			throw new RuntimeException("SVGUtils.getBBox: unsupported " + e.getClass().getName());
+		}
+	}
+
+}
