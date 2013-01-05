31677788b3c80c60fbf74141cdf9ae39cb43b937
rzymek <rzymek@gmail.com>
2013-01-05 22:06:33 +0100

    getOperation

---
+++ tools/earl/tools/GenOp.java
@@ -0,0 +1,25 @@
+package earl.tools;
+
+import java.io.File;
+import java.net.URL;
+
+import earl.client.bastogne.op.Move;
+
+public class GenOp {
+	public static void main(String[] args) {
+		URL url = Move.class.getResource(".");
+		File dir=  new File(url.getFile());
+		String[] listFiles = dir.list();
+		String tmpl ="if (%s.class.getName().equals(type)) {\n"+
+			"	return new %s();\n" +
+			"} else ";
+		for (String string : listFiles) {
+			String name = string.replace(".class","");
+			String s = String.format(tmpl, name, name);
+			System.out.print(s);
+		}
+		System.out.println("{");
+		System.out.println("	throw new EarlException(\"Unknow operation type:\" + type);");
+		System.out.println("}");
+	}
+}
