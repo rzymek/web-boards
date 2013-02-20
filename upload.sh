c43d6a3a6f30d293d4147085bdf99e00326d7380
rzymek <eerzymek@gmail.com>
2013-02-20 18:16:53 +0100

    update.sh params

--- upload.sh
+++ upload.sh
@@ -2,5 +2,5 @@ appid=`git rev-parse --abbrev-ref HEAD`
 if [ $appid = 'master' ]; then
 	appid=web-boards
 fi
-echo Updating $appid...
-mvn clean appengine:update -Pprod -Dappid=$appid
+echo "Updating $appid... $*"
+mvn clean appengine:update -Pprod -Dappid=$appid $*
