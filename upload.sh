9989fe56d142742f4bfcbb8f5d49636d25c8800d
rzymek <rzymek@gmail.com>
2013-02-23 18:58:11 +0100

    upload.sh - get ver from branch name

--- upload.sh
+++ upload.sh
@@ -1,6 +1,11 @@
-appid=`git rev-parse --abbrev-ref HEAD`
+appid=`git rev-parse --abbrev-ref HEAD |cut -d- -f1`
+ver=`git rev-parse --abbrev-ref HEAD |cut -d- -f2`
 if [ $appid = 'master' ]; then
-	appid=web-boards
+        appid=web-boards
 fi
-echo "Updating $appid... $*"
-mvn clean appengine:update -Pprod -Dappid=$appid $*
+if [ x"$ver" != x ];then
+        ver="-Dver=$ver"
+fi
+echo "Updating $appid... $ver $* "
+mvn clean appengine:update -Pprod -Dappid=$appid $ver $*
+
