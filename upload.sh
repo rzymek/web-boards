6756552be624769b9abb2a3f2ce3078bb85acc1f
rzymek <rzymek@gmail.com>
2013-02-25 23:41:12 +0100

    upload.sh: no version from branch name resolution

--- upload.sh
+++ upload.sh
@@ -1,10 +1,6 @@
 appid=`git rev-parse --abbrev-ref HEAD |cut -d- -f1`
-ver=`git rev-parse --abbrev-ref HEAD |cut -d- -f2`
 if [ $appid = 'master' ]; then
         appid=web-boards
-fi
-if [ x"$ver" != x ];then
-        ver="-Dver=$ver"
 fi
 echo "Updating $appid... $ver $* "
 mvn clean appengine:update -Pprod -Dappid=$appid $ver $*
