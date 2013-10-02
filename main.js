5d8db34c232530e6211e51133cf6d875b431f99d
rzymek <rzymek@gmail.com>
2013-10-02 21:46:07 +0200

    op tests

--- main.js
+++ main.js
@@ -1,15 +1,33 @@
+Operations = new Meteor.Collection('operations');
+
 if (Meteor.isClient) {
+    Ops = {ops:[]};
+    Ops.register = function(name, func) {
+        this.ops[name] = func;
+    }
+    
+    Ops.register('move', function(args) {
+        console.log("run: move: ",args);
+    });
+    
     Template.main.game = 'bastogne';
     Template.main.board = {w: 6800, h: 4400};
     Meteor.startup(function() {
         svgZAP.setup(document.getElementsByTagName("svg")[0]);
         console.log('started');
     });
+    Operations.find().observeChanges({
+       added: function(id,fields) {           
+           var op =  Ops.ops[fields.name];
+           op(fields.args);
+       }
+    });
 }
 
 if (Meteor.isServer) {
     Meteor.startup(function() {
-        // code to run on server at startup
+        Operations.remove({});
+        Operations.insert({name:'move', args:{what:'cnt', to:'5.35'}});
     });
 }
 
