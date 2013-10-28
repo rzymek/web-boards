fe9e94f11638c137c5e27964001b0967722f0dcb
rzymek <rzymek@gmail.com>
2013-10-28 23:10:01 +0100

    counter ids in pieces

--- client/templates/pieces.js
+++ client/templates/pieces.js
@@ -1,23 +1,21 @@
-/// <reference path="../TypedSession.api.d.ts"/>
-/// <reference path="../../packages/typescript-libs/jquery.d.ts" />
-/// <reference path="../api/core.d.ts"/>
-var pieces = Template['pieces'];
-pieces['categories'] = function () {
-    return [''].concat(S.gameInfo().pieces.map(function (pieces) {
+Template.pieces.categories = function() {
+    return [''].concat(S.gameInfo().pieces.map(function(pieces) {
         return pieces.category;
     }));
 };
-pieces.events({
-    'change select': function (e) {
+
+Template.pieces.selected = function() {
+    Session.equals('selectedPiece')
+};
+
+Template.pieces.events({
+    'change select': function(e) {
         var combo = e.target;
         var category = combo.options[combo.selectedIndex].value;
         S.setPiecesCategory(category);
         return true;
-    }
-});
-
-pieces.events({
-    'click img': function (e) {
+    },
+    'click img': function(e) {
         var img = e.currentTarget;
         $('#panel .panel-body img').removeClass('pieceSelected');
         $(img).addClass('pieceSelected');
@@ -25,18 +23,22 @@ pieces.events({
     }
 });
 
-Template['selectedPieces']['pieces'] = function () {
+Template.selectedPieces.pieces = function() {
     var g = S.selectedGame();
     var cat = S.piecesCategory();
-    var inCat = S.gameInfo().pieces.filter(function (p) {
+    var inCat = S.gameInfo().pieces.filter(function(p) {
         return p.category === cat;
     });
-    if (inCat.length > 0)
-        return inCat[0].list.map(function (p) {
+    if (inCat.length > 0) {
+        return inCat[0].list.map(function(p) {
             return p.images[0];
-        }).map(function (name) {
-            return '/games/' + g + '/images/' + name;
+        }).map(function(name) {
+            return {
+                src: '/games/' + g + '/images/' + name,
+                id: 'panel-'+name.replace(/[^A-Za-z0-9_]/,'')
+            };
         });
-else
+    } else {
         return [];
+    }
 };
