c1f6a67fcc16876245e84d7337ca5b6a847e7a6c
rzymek <rzymek@gmail.com>
2013-11-10 21:05:36 +0100

    getNatural

--- client/lib/dom-utils.js
+++ client/lib/dom-utils.js
@@ -5,3 +5,13 @@ byId = function(id) {
 isOnBoard = function(piece) {
     return piece && piece.parentElement && piece.parentElement.id !== 'piecesPanel';
 };
+
+getNatural = function (e) {
+    if (e.naturalWidth !== undefined && e.naturalHeight !== undefined) {
+        return {width: img.naturalWidth, height: img.naturalHeight};
+    } else {
+        var img = new Image();
+        img.src = e.src;
+        return {width: img.width, height: img.height};
+    }
+};
