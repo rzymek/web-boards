e4d1b9ee3854431f16243555f056f39b98dc68aa
rzymek <rzymek@gmail.com>
2014-03-01 22:07:13 +0100

    cleanup

--- client/load.gameInfo.js
+++ client/load.gameInfo.js
@@ -1,4 +1,4 @@
-Deps.autorun(function() {   
+Deps.autorun(function() {
     var tableId = Session.get('tableId');
     Session.set('gameInfo', defaultGameInfo);
     if(tableId === null) {
@@ -7,14 +7,6 @@ Deps.autorun(function() {
     }
     var table = Tables.findOne(tableId);
     $.get('/games/' + table.game + '/game.json', function(data) {
-//        var config = Session.get('config');
-//        if (config.indexOf('sfw') >= 0) {
-//            data.board.image = '../javadoc.png'; //TODO: remove
-//        } else if (config.indexOf('empty') >= 0) {
-//            data.board.image = '../../../empty.png'; //TODO: remove
-//        } else if (config.indexOf('hires') < 0) {
-//            data.board.image = '../board-low.jpg'; //TODO: remove
-//        }
         Session.set('gameInfo', data);
     });
 });
