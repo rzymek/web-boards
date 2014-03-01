072ffd5e3f3fd2dd13487595ce4538aedd35b8ba
rzymek <rzymek@gmail.com>
2014-03-01 20:53:49 +0100

    simplification: assume Tables are loaded here

--- client/load.gameInfo.js
+++ client/load.gameInfo.js
@@ -1,23 +1,20 @@
 Deps.autorun(function() {   
     var tableId = Session.get('tableId');
+    Session.set('gameInfo', defaultGameInfo);
     if(tableId === null) {
         Session.set('board.ready', false);
         return;
     }
     var table = Tables.findOne(tableId);
-    if(!table){
-        Session.set('gameInfo', defaultGameInfo);
-        return;
-    }    
     $.get('/games/' + table.game + '/game.json', function(data) {
-        var config = Session.get('config');
-        if (config.indexOf('sfw') >= 0) {
-            data.board.image = '../javadoc.png'; //TODO: remove
-        } else if (config.indexOf('empty') >= 0) {
-            data.board.image = '../../../empty.png'; //TODO: remove
-        } else if (config.indexOf('hires') < 0) {
-            data.board.image = '../board-low.jpg'; //TODO: remove
-        }
+//        var config = Session.get('config');
+//        if (config.indexOf('sfw') >= 0) {
+//            data.board.image = '../javadoc.png'; //TODO: remove
+//        } else if (config.indexOf('empty') >= 0) {
+//            data.board.image = '../../../empty.png'; //TODO: remove
+//        } else if (config.indexOf('hires') < 0) {
+//            data.board.image = '../board-low.jpg'; //TODO: remove
+//        }
         Session.set('gameInfo', data);
     });
 });
