05f28659fd6bfacd06d6d10e5c88c808f5afd4c5
rzymek <rzymek@gmail.com>
2014-03-04 22:12:05 +0100

    menu cleanup

--- client/menu.js
+++ client/menu.js
@@ -2,6 +2,9 @@ menu = {
     'Undo': function() {
         Meteor.call('undo');
     },
+    'Pieces': function() {
+        $('#panel').fadeToggle();
+    },
     'Back': function() {
         var idx = Session.get('replayIndex');
         if (idx === null)
@@ -11,9 +14,6 @@ menu = {
             return;
         Session.set('replayIndex', idx);
     },
-    'Pieces': function() {
-        $('#panel').fadeToggle();
-    },
     'Fwd': function() {
         var idx = Session.get('replayIndex');
         if (idx === null)
@@ -22,32 +22,6 @@ menu = {
         if (idx > Operations.find({}).count())
             idx = null;
         Session.set('replayIndex', idx);
-    },
-    'Flip': function() {
-        var selected = getSelectedId();
-        if (!selected)
-            return;
-        Operations.insert({
-            op: 'FlipOp',
-            counterId: selected
-        });
-    },
-    'Remove': function() {
-        var selected = getSelectedId();
-        if (!selected)
-            return;
-        Operations.insert({
-            op: 'DropOp',
-            counterId: selected
-        });
-    },
-    'Test': function() {
-        var meta = document.getElementsByTagName('meta')[0];
-        if(meta.content ==='')
-            meta.content = "initial-scale=1.0, user-scalable=no";
-        else
-            meta.content = '';
-        alert(meta.content);
     }
 };
 
