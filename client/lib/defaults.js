e8bab5d7bc2524f0ad90e08d2aae41748deb6274
rzymek <rzymek@gmail.com>
2014-03-07 00:07:47 +0100

    rm selectedGame

--- client/lib/defaults.js
+++ client/lib/defaults.js
@@ -1,7 +1,6 @@
 
 Session.setDefault('gameInfo', null);
 Session.setDefault('tableId', null);
-Session.setDefault('selectedGame', null);
 Session.setDefault('config', '');
 
 Session.set('board.ready', false);
