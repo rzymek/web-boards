8e7d3725f51c0d47cc0137a0090b7cb10b1faa60
rzymek <rzymek@gmail.com>
2014-03-11 19:36:18 +0100

    dep with log

--- dep.sh
+++ dep.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+git log > public/log
 meteor deploy wb.meteor.com #--debug
+echo 'Stable? [ENTER / Ctrl+C]'
+read
+meteor deploy wbs.meteor.com 
 meteor deploy web-boards.meteor.com
-#meteor deploy wbs.meteor.com 
