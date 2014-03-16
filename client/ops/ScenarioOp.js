8bc23c820312a8ca973390ad2f404f4db45175e1
rzymek <rzymek@gmail.com>
2014-03-17 00:26:23 +0100

    ScenatioOp (#19)

--- client/ops/ScenarioOp.js
+++ client/ops/ScenarioOp.js
@@ -2,24 +2,16 @@ ScenarioOp = function(data) {
     var scenos = Session.get('scenarios');
     var selected = data.name;
     var setup = scenos[selected];
-    var ids = [];
+    var undo = [];
     for (var i = 0; i < setup.length; i++) {
         var it = setup[i];
-        var id = '_' + i;
-        PlaceOp({
-            category: it[0],
-            name: it[1],
-            hexid: it[2],
-            _id: id
-        });
-        ids.push(id);
+        it._id = '_' + i;
+        undo.push(this[it.op](it));
     }
 
     return function() {
-        ids.forEach(function(id) {
-            RemovePieceOp({
-                counterId: id
-            });
+        undo.forEach(function(fn) {
+            fn();
         });
     };
 };
