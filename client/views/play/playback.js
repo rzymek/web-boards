48b45c3e886da64db512c67eca6c8a964873684e
rzymek <rzymek@gmail.com>
2014-03-30 22:39:20 +0200

    playback

--- client/views/play/playback.js
+++ client/views/play/playback.js
@@ -1,24 +1,58 @@
 Template.playback.is = function(name) {
     return is(name);
 };
-
+function getReplayIdx() {
+    var idx = Session.get('replayIndex');
+    return (idx == null)
+            ? idx = Operations.find({}).count()
+            : idx;
+}
 Template.playback.events({
     'click .rewind-to-start': function() {
         Session.set('replayIndex', 0);
     },
+    'click .play': function() {
+        $('.playback .play').hide();
+        $('.playback .pause').show();
+    },
+    'click .pause': function() {
+        $('.playback .play').show();
+        $('.playback .pause').hide();
+    },
     'click .fast-rewind': function() {
-        var idx = Session.get('replayIndex');
-        if (idx === null)
-            idx = Operations.find({}).count();
+        var idx = getReplayIdx();
+        var current = Operations.find({}, {
+            sort: {'createdAt': 1},
+            skip: idx - 1,
+            reactive: false
+        });
+        var rewindTo = Operations.findOne({
+            createdAt: {$lt: current.createdAt},
+            player: {$ne: current.player}
+        }, {reactive: false});
+        if (rewindTo == null) {
+            Session.set('replayIndex', 0);
+        } else {
+            var rewindBy = Operations.find({
+                $or: [
+                    {createdAt: {$gte: rewindTo.createdAt}},
+                    {createdAt: {$lt: current.createdAt}}
+                ]
+            }, {reactive: false}).count();
+            Session.set('replayIndex', idx - rewindBy);
+        }
+    },
+    'click .step-back': function() {
+        var idx = getReplayIdx();
         idx--;
         if (idx < 0)
             return;
         Session.set('replayIndex', idx);
     },
     'click .fast-forward': function() {
-        var idx = Session.get('replayIndex');
-        if (idx === null)
-            return;
+    },
+    'click .step-forward': function() {
+        var idx = getReplayIdx();
         idx++;
         if (idx > Operations.find({}).count())
             idx = null;
