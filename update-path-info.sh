26277dbece5adf872f5097318fe857185dabe983
rzymek <rzymek@gmail.com>
2014-04-16 22:41:53 +0200

    complete road paths

--- update-path-info.sh
+++ update-path-info.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 host=wb.meteor.com
 host=localhost:3000
-cd `dirname $0`/public/games/bastogne/
-wget http://$host/edit/bastogne/path-info.json
+wget http://$host/edit/bastogne/path-info.json -O `dirname $0`/public/games/bastogne/path-info.json
