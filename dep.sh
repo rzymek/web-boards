36c8283758e895fd6dec546daeabfe079fc48f27
rzymek <rzymek@gmail.com>
2014-04-22 20:16:22 +0200

    join login fixes. dep.sh: stable without developement dir

--- dep.sh
+++ dep.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+set -e -u
+cd `dirname $0`
 git log > public/log
 meteor deploy wb.meteor.com --debug
 echo 'Stable? [ENTER / Ctrl+C]'
 read
+trap "mv tests/developement ." EXIT
+mv developement tests/
 #meteor deploy wbs.meteor.com 
 meteor deploy web-boards.meteor.com
