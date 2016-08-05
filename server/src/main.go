5760f665a82d5cd60d3e9f2aad1fe9a4c65b42fa
rzymek <rzymek@gmail.com>
2016-08-05 20:05:06 +0200

    go json

--- server/src/main.go
+++ server/src/main.go
@@ -1,18 +1,42 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"encoding/json"
+	"net/http"
+	"net/url"
+	"bytes"
+//	"io"
 )
 
-func init() {
-    http.HandleFunc("/", handler)
+type Message struct {
+	URL *url.URL
+	Headers string
 }
 
+func init() {
+	http.HandleFunc("/", json_handler)
+}
+
+
+func json_handler(w http.ResponseWriter, r *http.Request) {
+	m := Message{
+		URL: r.URL,
+	}
+	var buffer bytes.Buffer
+	r.Header.Write(&buffer)
+	
+	b, err := json.Marshal(m)
+	if(err != nil) {
+		fmt.Println(err)
+	}else{
+		w.Write(b)
+	}	
+}
 func handler(w http.ResponseWriter, r *http.Request) {
- 	fmt.Fprintf(w, "Hi there: %s", r.URL.Path[1:])
- }
+	fmt.Fprintf(w, "Hi there: %s", )
+}
 
 func main() {
-    http.ListenAndServe(":8888", nil)
+	http.ListenAndServe(":8888", nil)
 }
