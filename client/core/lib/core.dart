1c697752d2a81efbac41b81d3536ecc865c9119b
rzymek <rzymek@gmail.com>
2016-08-05 21:06:10 +0200

    width=height=100%

--- client/core/lib/core.dart
+++ client/core/lib/core.dart
@@ -1,9 +1,12 @@
 library webboards_core;
 
 import 'dart:html';
+import 'dart:svg' as svg;
 import 'package:svg_zoom_and_pan/svg_zoom_and_pan.dart' as zoomAndPan;
 
 void init() {
-  var svg = query("svg");
+  svg.SvgSvgElement svg = query("svg");
   zoomAndPan.setupZoomAndPan(svg);
+  svg.attributes["width"] = "100%";
+  svg.attributes["height"] = "100%";
 }
